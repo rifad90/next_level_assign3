6009780653d761e73e99a71bcf1391764339218e
RK <reshmy@Reshmys-MacBook-Air.local>
2026-07-05 23:36:36 +0600

    query 3

--- query.sql
+++ query.sql
@@ -22,7 +22,13 @@ WHERE full_name ILIKE 'Tanvir%'
 -- =========================================================================
 -- Query 3
 -- =========================================================================
-
+SELECT
+    booking_id,
+    user_id,
+    match_id,
+    COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
 
 
 -- =========================================================================
